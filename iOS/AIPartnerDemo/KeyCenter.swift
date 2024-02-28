293e55fd0e361d00e7508ae840ba62250d06c31d
yiwangwuqianfjp <fan_jun_peng@163.com>
2024-02-28 12:26:20 +0800

    add keycenter

---
+++ iOS/AIPartnerDemo/KeyCenter.swift
@@ -0,0 +1,19 @@
+//
+//  KeyCenter.swift
+//  OpenLive
+//
+//  Created by GongYuhua on 6/25/16.
+//  Copyright © 2016 Agora. All rights reserved.
+//
+
+import Foundation
+import RTMTokenBuilder
+
+
+@objc class KeyCenter: NSObject {
+    
+    // MARK: - RTC
+    @objc static let APP_ID: String = <#appid#>
+    @objc static let APP_CERTIFICATE: String = <#certificate#>
+
+}
